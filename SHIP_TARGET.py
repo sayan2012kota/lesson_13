862b75740eadf94ca78910a515c18849c29f5f9f
sayan2012kota <sayankota2012@gmail.com>
2025-06-29 09:53:15 +0100

    Add files via upload

--- SHIP_TARGET.py
+++ SHIP_TARGET.py
@@ -20,12 +20,22 @@ def draw():
 def move_block():
     animate(second_char, "bounce_end", duration = 2, pos = next(position))
 
+def random_angle():
+    x = random.randint(100, 300)
+    y = random.randint(100, 500)
+    characters.target = x, y
+    target_angle = characters.angle_to(characters.target)
+    target_angle = target_angle + 360 * ((characters.angle - target_angle + 180) // 360)
+    animate(characters, angle = target_angle, duration = 0.2, on_finished = move_ship)
 
 move_block()
 clock.schedule_interval(move_block, 3)
 
 def move_ship():
-    animate(characters, tween = "accel_decel")
+    animate(characters, tween = "accel_decel", pos = characters.target, duration = characters.distance_to(characters.target) / 200, on_finished = random_angle)
 
+
+
+random_angle()
 
 pgzrun.go()
--- hw.py
+++ hw.py
@@ -12,6 +12,18 @@ pos_2 = itertools.cycle(SECOND_BLOCK_POSITIONS)
 
 characters = Actor("block", center = (200,300))
 second_char = Actor("block", center = (50, 50))
+third_char = Actor("bullet", center = (400, 600))
+
+def bullet_angle():
+    x = random.randint(0, 400)
+    y = random.randint(0, 600)
+    third_char.target = (x, y)
+    target_angle = third_char.angle_to(third_char.target)
+    animate(third_char, angle = target_angle, duration = 0.1, on_finished = move_bullet)
+
+def move_bullet(): 
+
+
 
 def draw():
     screen.clear()
